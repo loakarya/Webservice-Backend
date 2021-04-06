36985177b191c334656d474e4efc51b7776385c8
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-06 17:32:46 +0700

    [Add] Employees and comp. account migration schema

---
+++ database/migrations/2021_03_21_043223_create_company_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('name', 100);
+            $table->string('description', 200)->nullable();
+            $table->string('email', 200);
+            $table->string('username', 200)->nullable();
+            $table->string('password', 500);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_accounts');
+    }
+}
