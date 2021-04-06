1696d3ea08822c6b368d9eea9f5466d1d0b46dbb
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-06 17:32:13 +0700

    [Update] Commenting out the first db migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,24 +13,24 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name', 20);
-            $table->string('last_name', 50);
-            $table->string('email', 100)->unique();
-            $table->string('password', 200);
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('address', 200);
-            $table->string('zip_code', 10);
-            $table->string('city', 190);
-            $table->string('province', 90);
-            $table->string('country', 90);
-            $table->ipAddress('last_ip')->default('0.0.0.0');
-            $table->integer('acl')->unsigned()->default(0);
-            $table->rememberToken();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+        // Schema::create('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('first_name', 20);
+        //     $table->string('last_name', 50);
+        //     $table->string('email', 100)->unique();
+        //     $table->string('password', 200);
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->string('address', 200);
+        //     $table->string('zip_code', 10);
+        //     $table->string('city', 190);
+        //     $table->string('province', 90);
+        //     $table->string('country', 90);
+        //     $table->ipAddress('last_ip')->default('0.0.0.0');
+        //     $table->integer('acl')->unsigned()->default(0);
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        //     $table->softDeletes();
+        // });
     }
 
     /**
@@ -40,6 +40,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        // Schema::dropIfExists('users');
     }
 }
