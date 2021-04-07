c01b4aba18eeee27983b2c856cdfc74992e4c9f3
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-08 01:36:35 +0700

    [Update] Remaking add new employee feature

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Models\User;
 use App\Models\Employee;
+use App\Events\EmployeeRegistered;
 
 class EmployeeController extends Controller
 {
@@ -27,8 +30,18 @@ class EmployeeController extends Controller
         ]);
     }
 
-    public function store( Request $request, $id ) {
+    public function store( Request $request ) {
         $validation = Validator::make( $request->all(), [
+            'email' => 'required|email|max:100|unique:users,email',
+            'first_name' => 'required|max:20',
+            'last_name' => 'required|max:50',
+            'address' => 'required|max:200',
+            'zip_code' => 'required|max:10',
+            'city' => 'required|max:190',
+            'province' => 'required|max:90',
+            'country' => 'required|max:90',
+            'employee_id' => 'required|integer|gte:0|unique:employees,employee_id',
+            'private_email' => 'required|email|max:100|unique:employees,private_email',
             'bank_account_number' => 'required',
             'bank_account_provider' => 'required|max:100',
             'division' => 'required|max:100',
@@ -38,33 +51,45 @@ class EmployeeController extends Controller
         if ($validation->fails())
             return $this->sendValidationError( $validation->errors() );
 
-        $user = User::where( 'id', $id );
+        $user = new User;
+        $user->password = Hash::make(env('EMPLOYEE_DEFAULT_PASSWORD'));
+        $user->email = $request->email;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->address = $request->address;
+        $user->zip_code = $request->zip_code;
+        $user->city = $request->city;
+        $user->province = $request->province;
+        $user->country = $request->country;
+        $user->email_verified_at = now();
+        $user->acl = 1;
 
-        if ( $user->doesntExist() )
-            return $this->sendInvalidId('user');
+        $accountSaveStatus = $user->save();
 
-        $user = $user->first();
-
-        if ($user->employee()->exists())
-            return response()->json(['message' => 'There is a record exists for this user.'], 400);
+        if ( !$accountSaveStatus ) return response()->json(['message' => "Failed to save the new account."], 500);
 
         $employee = new Employee;
         $userCompanyEmailPassword = Str::random(40);
 
-        $employee->company_email_password = $userCompanyEmailPassword;
+        $employee->employee_id = $request->employee_id;
+        $employee->private_email = $request->private_email;
+        $employee->company_email_password = Crypt::encryptString($userCompanyEmailPassword);
         $employee->bank_account_number = $request->bank_account_number;
         $employee->bank_account_provider = $request->bank_account_provider;
         $employee->division = $request->division;
         $employee->title = $request->title;
+        
+        $employeeSaveStatus = $user->employee()->save( $employee );
+        
+        if ( !$employeeSaveStatus ) return response()->json(['message' => "Failed to save the employee's data."], 500);
+        
+        EmployeeRegistered::dispatch($employee);
 
-        if ( $user->employee()->save( $employee ) )
-            return response()->json([
-                'message' => "The employee's data has been saved.",
-                'data' => [
-                    'company_email_password' => $userCompanyEmailPassword
-                ]]);
-        else
-            return response()->json(['message' => "Failed to save the employee's data"], 500);
+        return response()->json([
+            'message' => "The employee's account has been created.",
+            'data' => [
+                'company_email_password' => $userCompanyEmailPassword
+            ]], 201);
     }
 
     public function patch() {
@@ -88,5 +113,63 @@ class EmployeeController extends Controller
                 ]]);
         else
             return response()->json(['message' => "Failed to randomize the employee's email password"], 500);
+    }
+
+    public function EmailLogIn() {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, env('ROUNDCUBE_URL'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, '');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, '');
+        $response = curl_exec($ch);
+
+        preg_match('|<input type="hidden" name="_token" value="([A-z0-9]*)">|', $response, $matches);
+
+        dd($response);
+
+        if ($matches) $token = $matches[1];
+        else return response()->json(['message' => 'Cannot parse the CSRF form login token.'], 500);
+
+        $email = auth()->user()->email;
+        $password = Crypt::decryptString(auth()->user()->employee->company_email_password);
+
+        $post_params = array(
+            '_token' => $token,
+            '_task' => 'login',
+            '_action' => 'login',
+            '_timezone' => '_default_',
+            '_url' => '_task=login',
+            '_user' => $email,
+            '_pass' => $password
+        );
+
+        curl_setopt($ch, CURLOPT_URL, "https://staff.loakarya.co/email/roundcube/". '?_task=login');
+        curl_setopt($ch, CURLOPT_COOKIEFILE, '');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, '');
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_params));
+        $response = curl_exec($ch);
+        $response_info = curl_getinfo($ch);
+        
+        if($response_info['http_code'] == 302)
+        {
+            // find all relevant cookies to set (php session + rc auth cookie)
+            preg_match_all('/set-cookie: (.*)\b/', $response, $cookies);
+            $cookie_return = array();
+
+            foreach($cookies[1] as $cookie)
+            {
+                preg_match('|([A-z0-9\_]*)=([A-z0-9\_\-]*);|', $cookie, $cookie_match);
+                if($cookie_match) {
+                    $cookie_return[$cookie_match[1]] = $cookie_match[2];
+                }
+            }
+
+            return response()->json(['token' => $cookie_return]);
+        }
+        else return response()->json(['message' => 'Failed to log in, please check your credentials.'], 401);
     }
 }
