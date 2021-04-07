a80b762a0089c1da50b38d5e5421b0a95d94837d
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-08 01:34:30 +0700

    [Add] Private email and employee id fields

--- database/migrations/2021_03_19_222714_create_employees_table.php
+++ database/migrations/2021_03_19_222714_create_employees_table.php
@@ -16,7 +16,9 @@ class CreateEmployeesTable extends Migration
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->string('company_email_password', 200);
+            $table->integer('employee_id')->unique();
+            $table->string('private_email', 100)->unique();
+            $table->string('company_email_password', 500);
             $table->integer('bank_account_number');
             $table->string('bank_account_provider', 100);
             $table->string('division', 100);
