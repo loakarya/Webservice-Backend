92648e33abdc785a387a52dd2bb05766c1865ff3
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-10 10:06:20 +0700

    [Add] Etc file config

---
+++ config/etc.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'employee_set_password_link' => env('EMPLOYEE_SET_PASSWORD_LINK'),
+    'employee_default_password' => env('EMPLOYEE_DEFAULT_PASSWORD'),
+    'roundcube_url' => env('ROUNDCUBE_URL')
+];
