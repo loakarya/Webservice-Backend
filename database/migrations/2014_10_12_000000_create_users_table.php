7bc72f1517b817c663b1edd02a3b05b62a25ce24
Fikri Rida Pebriansyah <fikriultimate18@gmail.com>
2021-04-14 01:31:23 +0700

    [Add] HR & marketing requested fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,9 @@ class CreateUsersTable extends Migration
             $table->string('city', 190);
             $table->string('province', 90);
             $table->string('country', 90);
+            $table->dateTime('birthday');
+            $table->boolean('gender');
+            $table->boolean('email_subs_agreement');
             $table->ipAddress('last_ip')->default('0.0.0.0');
             $table->integer('acl')->unsigned()->default(0);
             $table->rememberToken();
